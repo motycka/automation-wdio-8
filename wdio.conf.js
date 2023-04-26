f2356c5e2409ec928eed14e8ebdca96efe541bbf
Eva Vašicová <miev@seznam.cz>
2023-04-26 17:30:45 +0200

    lesson1

--- wdio.conf.js
+++ wdio.conf.js
@@ -45,7 +45,7 @@ export const config = {
     }],
     logLevel: 'silent',
     bail: 0,
-    baseUrl: 'ADRESA TESTOVANE APLIKACE',
+    baseUrl: 'https://team8-2022brno.herokuapp.com',
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
