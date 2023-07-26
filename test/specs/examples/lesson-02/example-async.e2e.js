87a4ff8301d138e1cb22e756715abf1f9ff6acf2
Eva Vašicová <miev@seznam.cz>
2023-07-26 22:36:59 +0200

    gdfdsgdf

--- test/specs/examples/lesson-02/example-async.e2e.js
+++ test/specs/examples/lesson-02/example-async.e2e.js
@@ -19,25 +19,25 @@ describe('Czechitas Login Page', async () => {
         /*
         Vypíše an object of type Element, protože jsme počkali na promise a ten byl resolvován
          */
-        console.log(await pageTitleSelector);
+        //console.log(await pageTitleSelector);
 
         /*
         Vypíše Promise { <pending> } protože jsme zavolali funkci getText() na neresolvovaném promisu
         */
-        console.log(pageTitleSelector.getText());
+        //console.log(pageTitleSelector.getText());
 
         /*
         Vypíše Promise { <pending> } protože jsme zavolali funkci na resolvovaném objektu ale nepočkali jsme
         na resoluci funkce getText()
         */
-        console.log((await pageTitleSelector).getText());
+        //console.log((await pageTitleSelector).getText());
 
         /*
         Vypíše "Přihlášení" protože jsme zavolali funkci a vyčkali jsem na resoluci celého řetězce promisů.
         To samé bychom dosáhli zavoláním:
         console.log(await (await pageTitleSelector).getText());
          */
-        console.log(await pageTitleSelector.getText());
+        //console.log(await pageTitleSelector.getText());
 
     });
 
