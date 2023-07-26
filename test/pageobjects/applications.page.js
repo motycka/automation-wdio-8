59b9d7d279da81587123dc2fbbf9b1903ed7c453
Eva Vašicová <117675887+matfyz-brno@users.noreply.github.com>
2023-07-26 22:20:33 +0200

    Update applications.page.js

--- test/pageobjects/applications.page.js
+++ test/pageobjects/applications.page.js
@@ -1,11 +1,28 @@
-class ApplicationsPage {
+class AppPage {
 
-    constructor() {
-        this.url = '/admin/prihlasky';
+    get toast() { return $('.toast-message'); }
+    get navbarRight() { return $('.navbar-right'); }
+    get userNameDropdown() { return this.navbarRight.$('[data-toggle="dropdown"]'); }
+    get logoutLink() { return $('#logout-link'); }
+
+    async open() {
+        await browser.url('/');
     }
 
-    // add page object functions here
+    async getToastMessage() {
+        return await this.toast.getText();
+    }
+
+    async logout() {
+        await this.userNameDropdown.click();
+        await this.logoutLink.click();
+    }
+
+    async getCurrentUser() {
+        return await this.userNameDropdown.getText();
+    }
 
 }
 
-export default new ApplicationsPage();
+// NOT a new instance!!!
+export default AppPage;
